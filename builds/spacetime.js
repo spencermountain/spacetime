4e4f98425b3768295c00d4b8649e5f9ace32b2f2
spencermountain <spencermountain@gmail.com>
2017-03-22 09:43:04 -0400

    remove deps

--- builds/spacetime.js
+++ builds/spacetime.js
@@ -1,4 +1,4 @@
-/* @smallwins/spacetime v0.0.1
+/* @smallwins/spacetime v0.0.2
   
 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.spacetime = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
@@ -2082,7 +2082,7 @@ module.exports={
 },{}],2:[function(_dereq_,module,exports){
 module.exports={
   "name": "@smallwins/spacetime",
-  "version": "0.0.1",
+  "version": "0.0.2",
   "description": "represent dates in remote timezones",
   "main": "./builds/spacetime.js",
   "license": "UNLICENSED",
@@ -2100,10 +2100,7 @@ module.exports={
   "files": [
     "builds/"
   ],
-  "dependencies": {
-    "cli-table": "^0.3.1",
-    "dst": "0.0.4"
-  },
+  "dependencies": {},
   "devDependencies": {
     "babel-preset-es2015": "6.9.0",
     "babel-preset-stage-2": "^6.11.0",
