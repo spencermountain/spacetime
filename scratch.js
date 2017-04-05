a725802f4bc27f01aa546b675710212b61e75ad3
spencermountain <spencermountain@gmail.com>
2017-04-05 17:59:15 -0400

    more comprehensive goto tests

--- scratch.js
+++ scratch.js
@@ -3,13 +3,22 @@
 const spacetime = require('./builds/spacetime');
 
 //bug. 1
-let s = spacetime('2016-01-01T9:00:00:122Z', 'Canada/Eastern');
-console.log(s.format().iso.local);
-
-spacetime.today();
+// let s = spacetime('2016-01-01T9:00:00:122Z', 'Canada/Eastern');
+// console.log(s.format().iso.local);
+//
+// spacetime.today();
 
 //bug 2.
 // let d = new Date('March 11, 2017');
 // let s = spacetime(d);
 // console.log(s.timezone());
 // s.log();
+
+let s = spacetime('February 22, 2017 14:42:00', 'Canada/Pacific');
+// s.goto('Canada/Pacific');
+// s.goto('Canada/Eastern');
+s.log();
+
+
+//eastern 5:37 PM
+//pacific 2:37 PM
--- test/goto.test.js
+++ test/goto.test.js
@@ -12,42 +12,64 @@ test('implicit goto', (t) => {
   t.end();
 });
 
-test('tz offset moves valid', (t) => {
+test('goto-from-est', (t) => {
   let s = spacetime('February 22, 2017 15:42:00', 'Canada/Eastern');
-  t.equal(s.date(), 22, 'date');
-  t.equal(s.monthName(), 'february', 'month');
-  t.equal(s.year(), 2017, 'year');
-  t.equal(s.hour(), 15, 'hour');
-  t.equal(s.minute(), 42, 'minute');
+  t.equal(s.date(), 22, 'est-date');
+  t.equal(s.monthName(), 'february', 'est-month');
+  t.equal(s.year(), 2017, 'est-year');
+  t.equal(s.hour(), 15, 'est-hour');
+  t.equal(s.minute(), 42, 'est-minute');
 
   //three hours back
   s.goto('Canada/Pacific');
-  t.equal(s.date(), 22, 'date');
-  t.equal(s.month(), 1, 'month');
-  t.equal(s.year(), 2017, 'year');
-  t.equal(s.hour(), 12, 'hour-moved');
-  t.equal(s.minute(), 42, 'minute');
+  t.equal(s.date(), 22, 'pst-date');
+  t.equal(s.month(), 1, 'pst-month');
+  t.equal(s.year(), 2017, 'pst-year');
+  t.equal(s.hour(), 12, 'pst-hour-moved');
+  t.equal(s.minute(), 42, 'pst-minute');
 
   //return to est
   s.goto('Canada/Eastern');
-  t.equal(s.date(), 22, 'date');
-  t.equal(s.month(), 1, 'month');
-  t.equal(s.year(), 2017, 'year');
-  t.equal(s.hour(), 15, 'hour-returned');
-  t.equal(s.minute(), 42, 'minute');
+  t.equal(s.date(), 22, 'est2-date');
+  t.equal(s.month(), 1, 'est2-month');
+  t.equal(s.year(), 2017, 'est2-year');
+  t.equal(s.hour(), 15, 'est2-hour-returned');
+  t.equal(s.minute(), 42, 'est2-minute');
+  t.end();
+});
 
-  //do a new date
-  s = spacetime('March 14, 2017 22:48:00');
+
+test('goto-from-algiers (no-dst-places)', (t) => {
+  //march 14th in algiers (+60)
+  let s = spacetime('March 14, 2017 22:48:00', 'Africa/Algiers');
+  t.equal(s.format().nice.long, 'Tuesday March 14th, 10:48pm', 'init-date');
+  //this shouldn't change things
   s.goto('Africa/Algiers');
-  t.equal(s.format().iso.short, '2017-02-15');
-  t.equal(s.format().time.h12, '3:48am');
-
-  //same thing for pacific time
-  s = spacetime('March 14, 2017 22:48:00');
-  s.goto('Canada/Pacific');
-  t.equal(s.format().iso.short, '2017-02-14');
-  t.equal(s.format().time.h12, '7:48pm');
-
+  t.equal(s.format().nice.long, 'Tuesday March 14th, 10:48pm', 'unchanged-date');
+  //same offset!
+  s.goto('Africa/Brazzaville');
+  t.equal(s.format().nice.long, 'Tuesday March 14th, 10:48pm', 'same-offset');
+  //one to the left...
+  s.goto('Africa/Dakar');
+  t.equal(s.format().nice.long, 'Tuesday March 14th, 9:48pm', 'one-hour-left');
+  //one to the right...
+  s.goto('Africa/Cairo');
+  t.equal(s.format().nice.long, 'Tuesday March 14th, 11:48pm', 'one-hour-right');
+  //two to the right...
+  s.goto('Asia/Baghdad');
+  t.equal(s.format().nice.long, 'Wednesday March 15th, 12:48am', 'two-hours-right-(tomorrow)');
+  //three to the right
+  s.goto('Asia/Dubai');
+  t.equal(s.format().nice.long, 'Wednesday March 15th, 1:48am', 'three-hours-right-(tomorrow)');
+  //three and a half to the right...
+  s.goto('Asia/Kabul');
+  t.equal(s.format().nice.long, 'Wednesday March 15th, 2:18am', 'three-and-a-half-to-the-right-(tomorrow)');
+  //back to yesterday..
+  s.goto('Africa/Dakar');
+  t.equal(s.format().nice.long, 'Tuesday March 14th, 9:48pm', 'back-to-one-hour-left');
+  //back to original
+  s.goto('Africa/Algiers');
+  t.equal(s.format().nice.long, 'Tuesday March 14th, 10:48pm', 'back-to-init-date');
 
   t.end();
 });
