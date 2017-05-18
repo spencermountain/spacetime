167d619399747bb436e4ba84b9be578e98d32241
spencermountain <spencermountain@gmail.com>
2017-05-18 19:04:24 -0400

    gh-pages demo

--- scripts/coverage.js
+++
@@ -1,7 +0,0 @@
-require('shelljs/global');
-var nyc = './node_modules/nyc/bin/nyc.js';
-var tape = './node_modules/tape/bin/tape';
-//run tests server-side
-// exec(nyc + ' ' + tape + ' \'./test/unit/**/*.test.js\'');
-exec(nyc + ' --reporter=html ' + tape + ' \'./test/**/*.test.js\'');
-// exec(nyc + ' --reporter=text-summary ' + tape + ' \'./test/unit/**/*.test.js\'');
