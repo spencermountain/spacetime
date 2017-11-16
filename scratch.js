ce26141a7ad0bae330ce1fa4e7ac08052e984b97
spencermountain <spencermountain@gmail.com>
2017-11-16 11:13:11 -0500

    support funnier numeric formats

--- scratch.js
+++ scratch.js
@@ -3,6 +3,12 @@ const spacetime = require('./src');
 // const moment = require('moment-timezone')
 // const spacetime = require('./builds/spacetime');
 
-let s = spacetime(1510799750000, 'Pacific/Auckland');
-s.log()
-console.log(s.timezone())
+// let s = spacetime(1510799750000, 'Pacific/Auckland');
+// s.log()
+// console.log(s.timezone())
+
+//all should be march 13th
+spacetime('2016/03/13').log()
+spacetime('03/13/2016').log()
+spacetime('2016/13/03').log()
+spacetime('13/03/2016').log()
