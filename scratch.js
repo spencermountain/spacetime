91c5b1e8d206a9fd7c4a883276a497bdf8e72cff
spencermountain <spencermountain@gmail.com>
2017-11-17 15:43:20 -0500

    tests passing !

--- scratch.js
+++ scratch.js
@@ -6,28 +6,10 @@ const spacetime = require('./src');
 // console.log(zones)
 
 
-//correct one
-// let jan = 1580299750000
-// var s = spacetime(jan, 'Africa/Abidjan')
-// s.log()
-// //incorrect one
-// let july = 1500299750000
-// var s = spacetime(july, 'Africa/Abidjan')
-// s.log()
-
-
-let s = spacetime('January 1, 2017 1:20:05', 'Canada/Eastern');
-console.log(s.hour())
-s.log()
-// var s = spacetime(1509358800000, 'Europe/Madrid');//want 11:20
-//WE WANT 6:20!!!
-// const epoch = 1509358800000
-// var s = spacetime(epoch, 'Canada/Eastern'); //want 6:20
-// s.log()
-// console.log(s.d.toLocaleTimeString())
-// console.log(s.timezone())
-// console.log(s.epoch === epoch)
-
-// === proper bias ==
-// 300  (5hrs)
-// new Date().getTimezoneOffset()
+//incorrect one
+for (let i = 0; i < 15; i++) {
+  var s = spacetime({
+    year: 2014 + i
+  }).endOf('year');
+  console.log(2014 + i, s.dayOfYear());
+}
