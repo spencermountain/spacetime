44d9e3c327355cf712793ba6c798cabccaec60e0
Brian LeRoux <b@brian.io>
2017-12-13 17:51:28 -0800

    playing around with an immutable ctor

---
+++ immutable.js
@@ -0,0 +1,32 @@
+var Spacetime = require('.')
+
+function clobber(instance) {
+  // these methods return values
+  let clobbers = ['add', 'subtract', 'hour', 'date', 'day', 'month', 'quarter', 'goto']
+  // walk thee clobbers and clobber with immutablilitieeeees
+  clobbers.forEach(prop=> {
+    var copy = instance.clone()
+    Object.defineProperty(instance, prop, {
+      enumerable: false,
+      value(...args) {
+        return copy[prop].apply(copy, args)
+      }
+    })
+  })
+  // this instance is unchanging
+  instance.clone = null
+  return instance
+}
+
+// a new factory; this one with terrible powers!
+function ImmutableSpacetime(...args) {
+  return clobber(new Spacetime(...args))
+}
+
+// copy helper factory ctor thingos
+ImmutableSpacetime.now = x=>clobber(Spacetime.now())
+ImmutableSpacetime.today = x=>clobber(Spacetime.today())
+ImmutableSpacetime.tomorrow = x=>clobber(Spacetime.tomorrow())
+
+// coolest sounding exports ever
+module.exports = ImmutableSpacetime
