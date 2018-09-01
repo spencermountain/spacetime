6eec671e038201bf5896c63c70ac8b6197f735a8
spencer kelly <spencermountain@gmail.com>
2018-09-01 13:28:54 -0400

    update deps

--- scratch.js
+++ scratch.js
@@ -1,13 +1,7 @@
 const spacetime = require('./src/index')
 
-var minus = spacetime('2018-07-09T12:59:00-07:00');
-console.log(minus.format('iso'));
-//
-// console.log('------------');
-//
-var plus = spacetime('2018-07-09T12:59:00+07:00');
-console.log(plus.format('iso'));
+// var minus = spacetime('2018-07-09T12:59:00-07:00');
+// console.log(minus.format('iso'));
 
-// var minus = spacetime.now('Etc/GMT-7');
-// minus.log()
-// console.log(minus.timezone())
+let s=spacetime.now().goto('Asia/Calcutta')
+console.log( s.format('iso'))
