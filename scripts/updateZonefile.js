80daf4727d90b528e225c76ef020aa40726b5dbf
spencer kelly <spencermountain@gmail.com>
2018-10-05 13:47:14 -0400

    try comparing offsets too

--- scripts/updateZonefile.js
+++ scripts/updateZonefile.js
@@ -44,12 +44,16 @@ const parseLine = (line) => {
   } else if (min === '0000' && dst === false) {
     hour += 1
   }
+  let offset = arr[arr.length - 1].replace(/gmtoff=/, '')
+  offset = parseInt(offset, 10) || 0
+  offset = offset / 60 / 60
   let obj = {
     month: zeroPad(months[arr[9].toLowerCase()]),
     date: zeroPad(parseInt(arr[10], 10)),
     hour: zeroPad(hour),
     min: min,
-    dst: dst
+    dst: dst,
+    offset: offset
   }
   if (obj.hour > 24 || obj.day > 31 || obj.month > 12) {
     console.error('oops', obj)
@@ -71,28 +75,41 @@ const parseTz = (tz) => {
     return null //console.log(' - no change for ' + tz)
   }
   lines = lines.map(parseLine)
+
   //remove 'end-of' dst changes
   lines = lines.filter((o) => o.min !== '5959' && o.min !== '2959') //urgh :/
   let a = lines[0]
   let b = lines[1]
   //this weird format i made: "03/26:03->10/29:02"
   let dst = `${a.month}/${a.date}:${a.hour}->${b.month}/${b.date}:${b.hour}`
-  return dst
+  return {
+    dst: dst,
+    offset: a.offset
+  }
 }
 
 const doAll = function() {
   let changes = 0
   let keys = Object.keys(data)
   keys.forEach((k) => {
-    let dst = parseTz(k)
-    if (dst && dst !== data[k].dst) {
-      console.log('\n----change ' + k + '----')
-      console.log('to: ' + dst)
-      console.log('was ' + data[k].dst)
-      console.log('')
-      let arr = k.split('/')
-      zonefile[arr[0]][arr[1]][2] = dst
-      changes += 1
+    let obj = parseTz(k)
+    if (obj) {
+      //compare offsets
+      if (obj.offset !== data[k].o) {
+        console.log('\n----offset change ' + k + '----')
+        console.log('to: ' + obj.offset)
+        console.log('from: ' + data[k].o)
+      }
+      //compare DST-change day
+      if (obj.dst !== data[k].dst) {
+        console.log('\n----DST change ' + k + '----')
+        console.log('to: ' + obj.dst)
+        console.log('was ' + data[k].dst)
+        console.log('')
+        let arr = k.split('/')
+        zonefile[arr[0]][arr[1]][2] = obj.dst
+        changes += 1
+      }
     }
   })
   console.log('\n\nmade ' + changes + ' changes to ' + keys.length + ' timezones')
