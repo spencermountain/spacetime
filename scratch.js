50baf455fa648424752384a6b32e6320c47f587e
spencer kelly <spencermountain@gmail.com>
2019-02-28 08:26:54 -0500

    fix ampm

--- scratch.js
+++ scratch.js
@@ -1,4 +1,10 @@
 const spacetime = require('./src/index')
 
-let s = spacetime('Tuesday August 1st, 12:00am')
+// let s = spacetime('Tuesday August 1st, 12:00am')
+// console.log(s.format('nice'))
+
+let s = spacetime('Aug 1st').time('12:00am')
+// s = s.hour(12)
+s = s.ampm('am')
+// s = s.ampm('pm')
 console.log(s.format('nice'))
