9dcba5f9cb11e137d2e96a724be8aa28d357ec1c
Jacob Craig <jacob.everett.craig@gmail.com>
2019-04-02 12:49:31 -0600

    Added comments

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,6 @@
 import { SpacetimeStatic } from "./constructors";
 
 declare const spacetime: SpacetimeStatic;
+
+// We need to use a single default export here so everything lines up with the actual imported object from JS
 export default spacetime;
