78dd126a6a109c19be939c3ab516cf479c3f804d
Jacob Craig <jacob.everett.craig@gmail.com>
2019-04-06 23:07:07 -0600

    Got the types tests working

--- test/types/index.ts
+++ test/types/index.ts
@@ -3,13 +3,21 @@
 // npm install -g ts-node
 // npm install --no-save @types/tape @types/node
 // npm run test:type
+
 import * as test from 'tape'
-import * as spacetime from '../../'
+import { spacetime } from './spacetime-static'
 
 test('typefile smoketest', (t: test.Test) => {
-  t.ok(true)
-
+  t.ok(spacetime, 'import works')
   const d = spacetime('June 5th 2019')
   t.equal(d.format('iso-short'), '2019-06-05', 'basic-smoketest')
+  t.end()
+})
+
+test('typefile static api exists', (t: test.Test) => {
+  t.equal(typeof spacetime, 'function', 'default is a function')
+  t.equal(typeof spacetime.now, 'function', '.now() is a function')
+  t.equal(typeof spacetime.today, 'function', '.today() is a function')
+  t.equal(typeof spacetime.tomorrow, 'function', '.tomorrow is a function')
   t.end()
 })
---
+++ test/types/spacetime-static.ts
@@ -0,0 +1,4 @@
+import * as spacetimejs from '../../builds/spacetime'
+import { SpacetimeStatic } from '../../types/constructors'
+
+export const spacetime: SpacetimeStatic = <any>spacetimejs
