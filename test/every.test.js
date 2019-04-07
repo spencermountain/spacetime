1a97c0fff94cd6dcc64b16f2e43e1ade899d6e16
spencer kelly <spencermountain@gmail.com>
2019-04-07 11:20:43 -0400

    failing every test

--- test/every.test.js
+++ test/every.test.js
@@ -7,7 +7,7 @@ test('every-unit', t => {
   let end = spacetime('April 20th 2019', 'Europe/Paris')
 
   let days = start.every('day', end)
-  t.equal(days.length, 13, '13 days')
+  t.equal(days.length, 14, '14 days')
   t.equal(days[0].timezone().name, 'Europe/Paris', 'results in right timezone')
 
   let weeks = start.every(' weEK ', end)
@@ -16,5 +16,14 @@ test('every-unit', t => {
   let years = start.every('years', end)
   t.equal(years.length, 0, '0 years')
 
+  t.end()
+})
+
+test('monday-sunday', t => {
+  let days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+  let start = spacetime('April 8th 2019').startOf('week')
+  let end = start.endOf('week')
+  let eachDay = start.every('day', end).map(d => d.dayName())
+  t.deepEqual(eachDay, days, 'got mon-sunday')
   t.end()
 })
