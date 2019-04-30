1c9eba78871e808007110d384a7bf2051770101c
martin.spodniak <martin.spodniak@accenture.com>
2019-04-30 13:19:25 +0200

    demo edited + undefined hotfix

---
+++ weekStarts-demo.js
@@ -0,0 +1,16 @@
+const weekStarts = require('./src/input/weekStarts');
+
+// currently zou have to supply as an argument name of the state, further version will 
+// find out the state itself. Name of the state can be in any case.
+// It finds out index of the day and uses days.long() to return a string
+// Can be used 3 letter short form of coutry name (ISO3166) or usual long name
+// https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+console.log(`
+First day of week in United States is ${weekStarts('UniTed STATES')}
+`);
+console.log(`
+First day of week in United Kingdom is ${weekStarts('gbr')}
+`);
+console.log(`
+First day of week in United Arab Emirates is ${weekStarts('aRaB eM')}
+`);
