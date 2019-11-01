3dfc2836cdfab69cc02cbe528ffa5d754e990236
spencer kelly <spencermountain@gmail.com>
2019-11-01 13:19:06 -0400

    add failing tests for #167

--- scratch.js
+++ scratch.js
@@ -1,5 +1,10 @@
 const spacetime = require('./src/index')
 
-let s = spacetime('12/01/2018', null, { dmy: true }) //jan 12th
-// let s = spacetime('18-02-2015')
-console.log(s.format('iso-short'))
+// let s = spacetime('October 6th 2019', 'australia/sydney') //.time('6:20pm')
+// s.log()
+// s = s.startOf('day')
+// s.log()
+
+console.log(spacetime('jan 1th 2018').week()) //tues
+console.log(spacetime('jan 9th 2018').week()) //wed
+console.log(spacetime('jan 15th 2018').week()) //tues
