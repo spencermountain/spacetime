44f34c5a33555e261d2d6a48cbfaf7f4c5c35f39
spencer kelly <spencermountain@gmail.com>
2020-10-03 11:14:57 -0400

    update deps and add code coverage action

--- scripts/coverage.js
+++
@@ -1,8 +0,0 @@
-const exec = require('shelljs').exec
-const codecov = '411de6c7-82d2-41e9-a1cc-9096cdab6c72' //i don't really care if you steal this.
-
-//let cmd=`./node_modules/.bin/nyc --reporter=text ./node_modules/.bin/tape ./test/**/*.test.js`
-//run all the tests
-const cmd = `./node_modules/.bin/nyc --reporter=text-lcov ./node_modules/.bin/tape ./test/**/*.test.js > coverage.lcov && ./node_modules/.bin/codecov -t ${codecov}`
-exec(cmd)
-console.log('\n 🏃 done!')
