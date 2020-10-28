2cf3aa1464abc1c59d6d0ca06c8e799d41332463
spencer kelly <spencermountain@gmail.com>
2020-10-28 18:24:51 -0400

    more dst-change examples working now

--- scratch.js
+++ scratch.js
@@ -1,16 +1,13 @@
 const spacetime = require('./src/index')
 
-// let s = spacetime([2020, 1, 29])
-// console.log(s.format())
+// let tz = 'america/indiana' // 11/05:01
+let tz = 'america/havana' // 11/01:01
 
-// let s = spacetime('now', null, { today: { year: 2012 } })
-// console.log(s.format())
-
-// 11/01:02
-let s = spacetime(`Nov 1st 2020 12:01am`, 'america/tijuana')
+// let s = spacetime(`Nov 5th 2020 12:01am`, tz)
+let s = spacetime(`Oct 31st 2020 11:51pm`, tz)
 for (let i = 0; i < 20; i += 1) {
   s = s.add(10, 'minutes')
   // console.log(s.time() + '   ' + s.timezone().current.isDST)
   console.log(s.format('{time} {nice}'), s.timezone().current.isDST)
 }
-console.log(s.timezones['america/tijuana'])
+console.log(s.timezones[tz])
--- test/dst-switch.test.js
+++ test/dst-switch.test.js
@@ -5,7 +5,10 @@ const spacetime = require('./lib')
 const getDstChange = (dstChange) => {
   let fall = dstChange.split('->')[1]
   const [month, rest] = fall.split('/')
-  const [day, hour] = rest.split(':')
+  let [day, hour] = rest.split(':')
+  if (hour === '24') {
+    hour = 0
+  }
   return {
     year: new Date().getFullYear(),
     month: Number(month) - 1, //
@@ -26,59 +29,34 @@ test('increment-to-dst-fall-change', (t) => {
     'america/thunder_bay',
     'america/nassau',
     'asia/famagusta',
-    'asia/beirut',
-    'america/indianapolis'
-    // working:
-    // 10/25:02   1hrs
-    // 10/25:03   2hrs
-    // 10/25:04   3hrs
-    // 10/25:04   3hrs
-    // 10/25:02   1hrs
-    // 11/01:02   -4hrs
-    // 11/01:02   -4hrs
-    // 10/25:04   3hrs
-    // 10/24:24   3hrs
-    // 11/01:02   -4hrs
+    'america/indianapolis',
+    'america/tijuana', //11/01:02  -7hrs
+    'mexico/bajanorte', //11/01:02  -7hrs
+    'america/havana',
+    'america/indiana',
+    'america/kentucky',
+    'america/north_dakota',
+    'america/scoresbysund',
+    'asia/amman',
+    'asia/gaza',
+    'asia/hebron',
+    'atlantic/azores'
 
     // not working
-
-    // 'asia/damascus' //10/29:24  3hrs
-    // 'america/tijuana' //11/01:02  -7hrs
-    // 'mexico/bajanorte' //11/01:02  -7hrs
-
-    // 1am changes
-    // 'america/havana',
-    // 'america/indiana',
-    // 'america/kentucky',
-    // 'america/north_dakota',
-    // 'america/scoresbysund',
-    // 'asia/amman',
-    // 'asia/gaza',
-    // 'asia/hebron',
-    // 'atlantic/azores'
-    // 11/01:01    -4hrs
-    // 11/05:01    -4hrs
-    // 11/05:01    -4hrs
-    // 11/05:01    -5hrs
-    // 10/25:01    0hrs
-    // 10/30:01    3hrs
-    // 10/31:01    3hrs
-    // 10/31:01    3hrs
-    // 10/25:01    0hrs
+    // 'asia/beirut' //10/24:24
+    // 'asia/damascus', //10/29:24  3hrs
   ]
   arr.forEach((tz) => {
     // get fall dst change
     let dstStr = spacetime().timezones[tz].dst
     let change = getDstChange(dstStr)
-    //create an object 2mins after
+    //create a date 2mins after dst change
     let after = spacetime(change, tz)
-    // create a time 2hrs before a dst change
-    change.hour -= 2
-    let before = spacetime(change, tz)
+    // create a time 2hrs before a dst change (-3hrs)
+    let before = after.clone().minus(3, 'hours')
     // start rolling towards the dst shift (but don't hit it)
     for (let i = 0; i < 12; i += 1) {
       let time = before.time()
-      // console.log(before.format('{time} {nice}'), before.timezone().current.isDST)
       t.equal(before.isBefore(after), true, time + ' before-change ' + tz)
       t.equal(before.timezone().current.isDST, true, time + ' dst-on ' + tz)
       before = before.add(10, 'minutes')
