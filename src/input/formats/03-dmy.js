8484d86156e0ac08778852d6e096bca1b711426a
spencer kelly <spencermountain@gmail.com>
2021-04-15 13:00:18 -0400

    all tests passing

--- src/input/formats/03-dmy.js
+++ src/input/formats/03-dmy.js
@@ -9,42 +9,42 @@ module.exports = [
   //common british format - "25-feb-2015"
   {
     reg: /^([0-9]{1,2})[\-\/]([a-z]+)[\-\/]?([0-9]{4})?$/i,
-    parse: (s, arr) => {
+    parse: (s, m) => {
       let obj = {
-        year: parseYear(arr[3], s._today),
-        month: parseMonth(arr[2]),
-        date: fns.toCardinal(arr[1] || '')
+        year: parseYear(m[3], s._today),
+        month: parseMonth(m[2]),
+        date: fns.toCardinal(m[1] || '')
       }
       if (validate(obj) === false) {
         s.epoch = null
         return s
       }
       walkTo(s, obj)
-      s = parseTime(s, arr[4])
+      s = parseTime(s, m[4])
       return s
     }
   },
   // "25 Mar 2015"
   {
     reg: /^([0-9]{1,2})( [a-z]+)( [0-9]{4}| '[0-9]{2})? ?([0-9]{1,2}:[0-9]{2}:?[0-9]{0,2}? ?(am|pm|gmt))?$/i,
-    parse: (s, arr) => {
+    parse: (s, m) => {
       let obj = {
-        year: parseYear(arr[3], s._today),
-        month: parseMonth(arr[2]),
-        date: fns.toCardinal(arr[1])
+        year: parseYear(m[3], s._today),
+        month: parseMonth(m[2]),
+        date: fns.toCardinal(m[1])
       }
       if (!obj.month || validate(obj) === false) {
         s.epoch = null
         return s
       }
       walkTo(s, obj)
-      s = parseTime(s, arr[4])
+      s = parseTime(s, m[4])
       return s
     }
   },
   // 01-jan-2020
   {
-    reg: /^([0-9]{1,2})[\. -/]([a-z]+)[\. -/]([0-9]{4})?$/i,
+    reg: /^([0-9]{1,2})[\. -/]([a-z]+)[\. -/]([0-9]{4})?( [0-9]{1,2}(:[0-9]{0,2})?(:[0-9]{0,3})? ?(am|pm)?)?$/i,
     parse: (s, m) => {
       let obj = {
         date: Number(m[1]),
@@ -57,6 +57,7 @@ module.exports = [
       }
       walkTo(s, obj)
       s = s.startOf('day')
+      s = parseTime(s, m[4])
       return s
     }
   }
