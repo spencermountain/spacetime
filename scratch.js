aeb718a18ea4a773ad0f6f5bd6a1eddd6b933da9
spencer kelly <spencerkelly86@gmail.com>
2023-03-23 08:52:33 -0400

    progress

--- scratch.js
+++ scratch.js
@@ -3,23 +3,12 @@ import { DateTime } from "luxon";
 // import old from './old/src/index.js'
 
 
-// spacetime.world.methods.now = () => 1554092400000
-
-// console.log(spacetime({ year: 1983 }).iso())
-
-// spacetime.world.model.days[1].longForm = 'Lundi'
-// console.log(spacetime.world.model.days)
-// console.log(spacetime.now().day(1).format('day'))
-
-// console.log(spacetime([1999, 12, 31]).dayName())
-
-// s = s.add(22, 'minute').minus(44, 'minute').add(22, 'minute')
-// console.log(s.iso())
-
 
 // let s = spacetime('2021-01-01T00:00:00.000+02:30')
 // s = s.add(22, 'minute').minus(44, 'minute').add(22, 'minute')
 // console.log(s.iso())
 
 let s = spacetime.now()
-console.log(s.hour())
+console.log(s.progress())
+// console.log(new Date().toLocaleString())
+// console.log(s.hour())
--- src/01-one/api/progress.js
+++ src/01-one/api/progress.js
@@ -1,4 +1,18 @@
-const progress = function (s, unit) {
-  return {}
+const percent = (part, total) => {
+  let num = (part / total) * 100;
+  num = Math.round(num * 10) / 10;
+  return num;
+};
+
+const units = ['year', 'month', 'date', 'hour', 'minute', 'second']
+const progress = function (unit) {
+  return units.reduce((h, u) => {
+    let start = this.startOf(u)
+    let end = this.next(u)
+    let total = end.epoch - start.epoch
+    let n = this.epoch - start.epoch
+    h[u] = percent(n, total)
+    return h
+  }, {})
 }
 export default progress
