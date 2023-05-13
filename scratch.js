574e1a955f7aece22c360f4409a4df6a4c19ab8e
DarshanDixit05 <darshandixit0@gmail.com>
2023-05-14 05:26:06 +0530

    bug fix : Times of the day seem illogical

--- scratch.js
+++ scratch.js
@@ -1,13 +1,12 @@
 // const spacetime = require('./builds/spacetime.cjs')
 import spacetime from './src/index.js'
 
-// spacetime.extend(require('./plugins/holiday'))
-
-// let s = spacetime()
+let s = spacetime()
 // s = s.year(2020)
 // s = s.dayOfYear(366);
-// console.log(s.json())
+// console.log(s.dayTime());
+console.log(s.json())  
 
-let d = spacetime('2022/01/01','utc');
-d = d.every('day','2022/12/31');
-d.forEach(function (e) {console.log(e.format('iso-short'),e.week())})
+// let d = spacetime('2022/01/01','utc');
+// d = d.every('day','2022/12/31');
+// d.forEach(function (e) {console.log(e.format('iso-short'),e.week())})
