d55b0e6d1a47ba803653f6cae12f32e93c9aeebc
Michael Naumov <mnaoumov@gmail.com>
2025-02-10 14:01:23 -0600

    Fix typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,11 @@
-import type { SpacetimeStatic } from './constructors'
+import type { SpacetimeStatic } from './constructors.js'
 
 declare const spacetime: SpacetimeStatic
 
 // We need to use a single default export here so everything lines up with the actual imported object from JS
 export default spacetime
 
-export { SpacetimeConstructor, SpacetimeConstructorOptions, SpacetimeStatic } from './constructors'
-export { Format, I18nOptions, TimeUnit } from './constraints'
-export { Spacetime, Diff, ParsableDate, Progress, Since, TimezoneMeta, TimezoneSet } from './types'
+
+export { SpacetimeConstructor, SpacetimeConstructorOptions, SpacetimeStatic } from './constructors.js'
+export { Format, I18nOptions, TimeUnit } from './constraints.js'
+export { Spacetime, Diff, ParsableDate, Progress, Since, TimezoneMeta, TimezoneSet } from './types.js'
