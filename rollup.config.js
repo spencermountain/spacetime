ab5cf1bef2d1253c0a936173f6c32e214bdf36c0
spencer kelly <spencerkelly86@gmail.com>
2026-05-27 15:32:27 -0400

    update deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
-import commonjs from 'rollup-plugin-commonjs'
-import json from 'rollup-plugin-json'
-import { terser } from 'rollup-plugin-terser'
-import resolve from 'rollup-plugin-node-resolve'
-import sizeCheck from 'rollup-plugin-filesize-check'
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import terser from '@rollup/plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import fs from 'fs'
 
 const pkg = JSON.parse(fs.readFileSync('./package.json').toString())
@@ -15,7 +14,7 @@ export default [
   {
     input: 'src/index.js',
     output: [{ banner: banner, file: 'builds/spacetime.mjs', format: 'esm' }],
-    plugins: [resolve(), json(), terser(), sizeCheck({ expect: 48, warn: 10 })]
+    plugins: [nodeResolve(), json(), terser()]
   },
   {
     input: 'src/index.js',
@@ -29,21 +28,19 @@ export default [
       }
     ],
     plugins: [
-      resolve(),
+      nodeResolve(),
       json(),
       commonjs(),
-      sizeCheck({ expect: 110, warn: 10 })
     ]
   },
   {
     input: 'src/index.js',
     output: [{ banner: banner, file: 'builds/spacetime.min.js', format: 'umd', name: 'spacetime' }],
     plugins: [
-      resolve(),
+      nodeResolve(),
       json(),
       commonjs(),
       terser(),
-      sizeCheck({ expect: 48, warn: 10 })
     ]
   }
 ]
