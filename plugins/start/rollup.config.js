3a3ff68769a974f03801e01a43c1c52abb4d75cb
spencer kelly <spencerkelly86@gmail.com>
2026-07-11 11:00:22 -0400

    publish

--- plugins/start/rollup.config.js
+++ plugins/start/rollup.config.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser';
 const pkg = JSON.parse(fs.readFileSync('./package.json').toString())
 
 console.log('\n 📦  - running rollup..\n')
@@ -10,11 +10,13 @@ const banner = `/* spencermountain/${name} ${pkg.version} MIT */`
 export default [
   {
     input: 'src/index.js',
-    output: [{ banner: banner, file: `builds/${name}.mjs`, format: 'esm' }]
+    output: [{ banner: banner, file: `builds/${name}.mjs`, format: 'esm' }],
+    plugins: [terser()]
   },
   {
     input: 'src/index.js',
-    output: [{ banner: banner, file: `builds/${name}.cjs`, format: 'umd', sourcemap: false, name: 'spacetimeStart' }]
+    output: [{ banner: banner, file: `builds/${name}.cjs`, format: 'umd', sourcemap: false, name: 'spacetimeStart' }],
+    plugins: [terser()]
   },
   {
     input: 'src/index.js',
