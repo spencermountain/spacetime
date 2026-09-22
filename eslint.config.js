dc45f77665f81b267ec09d53235598ab272b383e
Spencer Kelly <spencermountain@gmail.com>
2026-09-22 13:46:56 -0400

    linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,65 +1,70 @@
 import * as regexpPlugin from "eslint-plugin-regexp"
-  const ok = 'readonly'
 
-  export default [
-  regexpPlugin.configs["flat/recommended"],
-    { ignores: ['**/builds/*'] },
-    {
-      languageOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        globals: {
-          // Node globals
-          console: ok,
-          process: ok,
-          Buffer: ok,
-          setTimeout: ok,
-          clearTimeout: ok,
-          setInterval: ok,
-          clearInterval: ok,
-          __dirname: ok,
-          __filename: ok,
-          // client-side globals
-          self: ok,
-          window: ok,
-          document: ok,
-          navigator: ok,
-          fetch: ok,
-          URL: ok,
-          Event: ok
-        }
-      },
-      // custom rules setup
-      rules: {
-        'no-unused-vars': 'warn',
-        'no-empty': 'warn',
-        'no-undef': 'error',
-        'no-unreachable': 'error',
-        'no-dupe-keys': 'error',
-        'constructor-super': 'error',
-        'no-this-before-super': 'error',
-
-        'comma-dangle': ['warn', 'only-multiline'],
-        'max-nested-callbacks': ['warn', 4],
-        'max-params': ['warn', 5],
-        'consistent-return': 'warn',
-        'no-nested-ternary': 'warn',
-        'no-bitwise': 'warn',
-        'no-console': 'warn',
-        'no-duplicate-imports': 'warn',
-        'no-eval': 'error',
-        'no-implied-eval': 'error',
-        'no-mixed-operators': 'error',
-        'no-multi-assign': 'error',
-        'no-self-compare': 'warn',
-        'no-sequences': 'warn',
-        'no-shadow': 'error',
-        'no-unmodified-loop-condition': 'warn',
-        'no-use-before-define': 'warn',
-        'prefer-const': 'warn',
-        radix: 'warn',
-        // regex
-        'regexp/prefer-d': 'off'
+export default [
+  { ignores: ['**/builds/*', '**/scratch.js', '**/rollup.config.js'] },
+  regexpPlugin.configs['flat/recommended'],
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        // Node globals
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        // client-side globals
+        self: 'readonly',
+        window: 'readonly',
+        document: 'readonly',
+        fetch: 'readonly',
+        URL: 'readonly',
+        Event: 'readonly'
       }
+    },
+    // custom rules setup
+    rules: {
+      'no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_'
+        }
+      ],
+      'no-empty': 'warn',
+      'no-undef': 'error',
+      'no-unreachable': 'error',
+      'no-dupe-keys': 'error',
+      'constructor-super': 'error',
+      'no-this-before-super': 'error',
+
+      'comma-dangle': ['warn', 'only-multiline'],
+      'max-nested-callbacks': ['warn', 4],
+      'max-params': ['warn', 5],
+      'consistent-return': 'warn',
+      'no-nested-ternary': 'warn',
+      'no-bitwise': 'warn',
+      'no-duplicate-imports': 'warn',
+      'no-eval': 'error',
+      'no-implied-eval': 'error',
+      'no-multi-assign': 'error',
+      'no-self-compare': 'warn',
+      'no-sequences': 'warn',
+      radix: 'warn',
+      'no-shadow': 'error',
+      'no-unmodified-loop-condition': 'warn',
+      'no-use-before-define': 'warn',
+      'no-irregular-whitespace': 'warn',
+      'no-console': 'off',
+      'no-mixed-operators': 'off',
+      'no-prototype-builtins': 'off',
+      'prefer-const': 'warn',
+      'regexp/prefer-d': 'off'
     }
-  ]
+  }
+]
